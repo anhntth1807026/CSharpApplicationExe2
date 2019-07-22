edb6f77f171a4a386305e1b42e90f2d73d1f715e
anhntth1807026 <anhntth1807026@fpt.edu.vn>
2019-07-22 15:29:45 +0700

    Exe2

---
+++ ApplicationProgrammingEx2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ApplicationProgrammingEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String: ");
+            Console.WriteLine("Aptech Computer Center: ");
+            var _string = Console.ReadLine();
+            Console.WriteLine("Enter a word to search: ");
+            var _word = Console.ReadLine();
+            var _found = Regex.Matches(_string, _word).Count;
+            Console.WriteLine("Word found {0} times in the string", _found);
+        }
+    }
+}
